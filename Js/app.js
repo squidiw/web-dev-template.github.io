57726ad519963b025429819ab65d1139048878d2
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-04-09 14:37:47 +0000

    added aos

--- Js/app.js
+++ Js/app.js
@@ -1,15 +1,26 @@
+// PORTFOLIO SPLIT SCROLLING
+
 function splitScroll() {
-    const controller = new ScrollMagic.Controller();
+   const controller = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene({
-        duration : '260%',
-        triggerElement: '.portfolio-title',
-        triggerHook: 0
-    })
-    .setPin('.portfolio-title')
-    // .addIndicators()
-    .addTo(controller);
-
+   new ScrollMagic.Scene({
+      duration: "260%",
+      triggerElement: ".portfolio-title",
+      triggerHook: 0,
+   })
+      .setPin(".portfolio-title")
+      // .addIndicators()
+      .addTo(controller);
 }
 
 splitScroll();
+
+
+
+// SET YEAR IN COPYRIGHT
+let copyrightDate = document.getElementById("copyrightDate");
+
+let date = new Date();
+let currentYear = date.getFullYear();
+
+copyrightDate.innerHTML = currentYear;
