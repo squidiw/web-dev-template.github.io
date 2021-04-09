c02f3e94887fee9638a01977105fa8ec4faf3d70
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-04-09 10:44:31 +0000

    changed footer, added bitmoji and new text

--- Js/app.js
+++ Js/app.js
@@ -2,12 +2,12 @@ function splitScroll() {
     const controller = new ScrollMagic.Controller();
 
     new ScrollMagic.Scene({
-        duration : '300%',
+        duration : '260%',
         triggerElement: '.portfolio-title',
         triggerHook: 0
     })
     .setPin('.portfolio-title')
-    .addIndicators()
+    // .addIndicators()
     .addTo(controller);
 
 }
